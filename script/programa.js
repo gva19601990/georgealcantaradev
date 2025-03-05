0d877cbc92a9d570b3eed044161d8b229af940d3
George Vasconcelos Alcantara <151486904+gva19601990@users.noreply.github.com>
2025-03-04 21:58:07 -0300

    Add files via upload

--- script/programa.js
+++ script/programa.js
@@ -1,5 +1,4 @@
 
-
 function mudoutamanho() {
     if (window.innerWidth >= 768){itens.style.display ='block'}
     else {itens.style.display='none'}    
@@ -10,3 +9,5 @@ function clickMenu() {
     if (itens.style.display == 'block'){itens.style.display ='none'}
     else {itens.style.display='block'}     
     }
+
+
